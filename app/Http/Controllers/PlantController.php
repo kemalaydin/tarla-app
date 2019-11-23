f31b6ac50cba5b005f667a7fb7340e24d54c7d9a
Mehmet <mehmet.kaynk@gmail.com>
2019-11-23 16:55:50 +0300

    edit son

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -15,10 +15,6 @@ class PlantController extends Controller
      */
     public function index()
     {
-        $username = "kemal";
-=======
-        $user = "kemal";
->>>>>>> Stashed changes
         $Plants = Plantation::get();
         return view('plant.index',compact('Plants'));
     }
